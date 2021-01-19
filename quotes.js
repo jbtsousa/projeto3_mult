56534feecd06c18729d69cda0a7e3aa4fcc89e30
jbtsousa <jbtsousa@gmail.com>
2021-01-19 14:41:27 +0000

    added sound to sections, fixed comboio, some more minor adjustments

--- quotes.js
+++ quotes.js
@@ -15,7 +15,6 @@ var sec = document.getElementById("part2-text1");
 
 let counter = 0;
 
-
 setInterval(function(){
   q.innerHTML = quotes[counter];
 
@@ -26,27 +25,25 @@ setInterval(function(){
     q.style.opacity = 1;
   }
 
-  
   var positionInfo = sec.getBoundingClientRect();
   var secHeight = positionInfo.height/3;
   var secWidth = positionInfo.width;
-  
+
   qHeight = q.clientHeight;
   qWidth = q.clientWidth;
-  
+
   availSpace_V = secHeight - qHeight;
   availSpace_H = secWidth - qWidth;
 
   var randNum_V = Math.round(Math.random() * availSpace_V);
   var randNum_H = Math.round(Math.random() * availSpace_H);
-                
-  
+
+
   q.style.top = randNum_V + "px";
   q.style.left = randNum_H + "px";
-  
+
   counter++;
   if (counter >= quotes.length) {
     counter = 0;
-  }  
+  }
 }, 2000);
-
